77a4c4575ee608336083adec863b8b7261161cec
Kang7964 <stare5678@gmail.com>
2026-05-17 21:53:04 +0800

    Update cloud-config.js

--- cloud-config.js
+++ cloud-config.js
@@ -1,2 +1,2 @@
 // Paste your Google Apps Script Web App URL between the quotes after deployment.
-window.AQUA_LASH_API_URL = "https://script.google.com/macros/s/AKfycbwVBV9W72_CSkS44kxf5PGUbbEaGtlHh1dJXUufuBaof5qG-PUK7YeM-qn3nekXKxnx/exec";
+window.AQUA_LASH_API_URL = "https://script.google.com/macros/library/d/1k_ROvjG-bkaxTIGyrOUwRQH2dTdp8VNHMyYEPE7lwXud0uqya0-KkSvP/2";
